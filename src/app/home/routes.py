6288bfb6b124aad2046ab789c4fa7f7a91cad2ad
Eddie <2002226@sit.singaporetech.edu.sg>
2021-11-27 01:21:37 +0800

    Change submit Queue Commands to DB

--- src/app/home/routes.py
+++ src/app/home/routes.py
@@ -3,12 +3,16 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from os import abort, error
+from flask.blueprints import Blueprint
+from flask.helpers import flash
 from flask.json import jsonify
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+import sqlite3
 
 @blueprint.route('/index')
 @login_required
@@ -41,14 +45,10 @@ def route_template(template):
 def get_segment( request ): 
 
     try:
-
         segment = request.path.split('/')[-1]
-
         if segment == '':
             segment = 'index'
-
         return segment    
-
     except:
         return None  
 
@@ -65,17 +65,72 @@ def getCommands():
         return jsonify(commands)
     return jsonify("")
 
-@blueprint.route('/api/car/commands', methods=["GET", "POST"])
+@blueprint.route('/submitCommands', methods=["GET", "POST"])
 def submitQueue():
     if request.method == "GET":
-        #Get the queue from AJAX GET request
-        queue = request.args.get('qCommands')
-        print("Command",queue)
-        return queue + "\0"
-    return jsonify("")
+        try:
+            # Establish database Connection
+            conn = sqlite3.connect('db.sqlite3')
+            c = conn.cursor()
+        except:
+            return "Fail to connect to database"
+        try:
+            #Get the queue from AJAX GET request
+            queue = request.args.get('qCommands')
+            for q in queue:
+                #Insert data to DB here
+                c.execute("INSERT INTO Queue(Commands) VALUES(?)", (q,))
+            conn.commit()
+            conn.close()
+            return "Success"
+        except:
+            return "Fail to submit queue commands"
+    return "Fail"
+
+@blueprint.route('/api/commands/deqeue', methods=["GET", "POST"])
+def dequeue():
+    if request.method == "GET":
+        # Establish database Connection
+        try:
+            conn = sqlite3.connect('db.sqlite3')
+            c = conn.cursor()
+        except:
+            return "Fail to connect to database"
+        try:
+            #Get the queue from AJAX GET request
+            c.execute("SELECT commands FROM Queue ORDER BY QueueID ASC LIMIT 1")
+            data = c.fetchall()
+            c.execute("DELETE FROM Queue WHERE QueueID = (SELECT QueueID FROM Queue ORDER BY QueueID ASC LIMIT 1)")
+            conn.commit()
+            conn.close()
+            return data[0][0]
+        except:
+            # flash("No commands in queue")
+            # return render_template('page-500.html'), 500
+            return "No commands in queue"
+    return "Fail"
+
+
+@blueprint.route('/api/commands/getFirstCommands', methods=["GET", "POST"])
+def getFirstCommand():
+    if request.method == "GET":
+        # Establish database Connection
+        conn = sqlite3.connect('db.sqlite3')
+        # conn.close()
+        c = conn.cursor()
+        try:
+            #Get the queue from AJAX GET request
+            c.execute("SELECT commands FROM Queue ORDER BY QueueID ASC LIMIT 1")
+            data = c.fetchall()
+            conn.close()
+            return data[0][0]
+        except:
+            # flash("No commands in queue")
+            # return render_template('page-500.html'), 500
+            return "No commands in queue"
+    return "Fail"
 
 @blueprint.route("/espmodule", methods=['GET'])
 def helloHandler():
     if request.method == 'GET':
         return 'Hello EcSP8266'
-        
