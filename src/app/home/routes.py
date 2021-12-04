f681421397c39f95ba473ba77261d99a5c1dcaa3
DanielFYJ <2000864@sit.singaporetech.edu.sg>
2021-12-05 03:20:59 +0800

    Dashboard currently set to retrieve data from db every 10seconds

--- src/app/home/routes.py
+++ src/app/home/routes.py
@@ -1,14 +1,29 @@
 # -*- encoding: utf-8 -*-
+# DELETE FROM Data WHERE dataID IN(1,2)
 """
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from sqlite3.dbapi2 import Cursor
 from flask.json import jsonify
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, Flask,request, make_response
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+import random
+import json
+from time import time
+import sqlite3
+
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('Database.db')
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 @blueprint.route('/index')
 @login_required
@@ -73,4 +88,39 @@ def submitQueue():
         queue = request.args.get('qCommands')
         return jsonify(queue)
     return jsonify("")
-        
+@blueprint.route('/Dashboard', methods=["GET", "POST"])
+def main():
+    return render_template('Dashboard.html')
+
+count = 0 
+@blueprint.route('/testdata', methods=["GET", "POST"])
+def data():
+    # This one is to 1 by 1 increment each run of the function
+    # global count
+    # count += 1
+    # str(count)
+
+    # Random value
+    carid = random.randint(1, 4)
+    # print(carid)
+    
+    if request.method == "GET":
+        # Establish database Connection
+        conn = sqlite3.connect('Database.db')
+        # conn.close()
+        c = conn.cursor()
+        try:
+            c.execute("SELECT * FROM Data WHERE dataID=" +str(carid))
+            for row in c:
+                carmovement = row[1]
+                speed = row[2]
+            data = [time() * 1000, carmovement, speed]
+            # data = c.fetchall()
+            conn.close()
+            response = make_response(json.dumps(data))
+            response.content_type = 'application/json'
+            return (response)     
+        except:
+            return "No data" + '\0'
+    return "Fail"
+
